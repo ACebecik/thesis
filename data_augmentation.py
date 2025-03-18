added 0-2 scaling augmentation for the fixed data

data_augmentation.py
@@ -10,66 +10,41 @@ from tqdm import tqdm
 
 
 # load the data from saved tensors
-X_mit = torch.load("tensors/mit_all_records_X_w120_fixed.pt")
-y_mit = torch.load("tensors/mit_all_records_y_w360.pt")
+um_train_X = torch.load("tensors/final_tensors_1703/um_train_X.pt")
+um_train_y = torch.load("tensors/final_tensors_1703/um_train_y.pt")
+um_reference_train_X = torch.load("tensors/final_tensors_1703/um_reference_train_X.pt")
 
 
-X_unovis = torch.load("tensors/unovis_all_records_X_w120.pt")
-y_unovis = torch.load("tensors/unovis_all_records_y_w120.pt")
+# Data Augmentation
 
-print(y_mit.shape)
-print(y_unovis.shape)
+print(f"Old cd for y_train:{sum(um_train_y)/um_train_y.shape[0] } ") 
+init_train_length = um_train_y.shape[0]
 
-X = np.vstack((X_mit, X_unovis))
-y = np.concatenate((y_mit, y_unovis))
-
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2)
-
-# Data Augmentation so that the class distribution is balanced
-# 
-
-print(f"Old cd for y_train:{sum(y_train)/y_train.shape[0] } ") 
-init_train_length = y_train.shape[0]
-
-for segment in tqdm(range(init_train_length//2)):
-    if y_train[segment] == 1:
-        scaler = MinMaxScaler(feature_range=(0,2))
-        scaled_X_segment = scaler.fit_transform(X_train[segment, :].reshape(-1,1))
-       # print(scaled_X_segment.shape)
-       # print(X_train.shape, y_train.shape)
-
-        X_train = np.vstack((X_train, scaled_X_segment.squeeze()))         
-        y_train = np.hstack((y_train, np.array(1))) 
-
-       # print(X_train.shape, y_train.shape)
-        
-
-print(f"New cd for y_train:{sum(y_train)/y_train.shape[0] } ")
-
-torch.save(X_train, "tensors/augmented_UM_train_X.pt")
-torch.save(y_train, "tensors/augmented UM_train_y.pt")
-
-# test data augmentation
+augmented_train_segments_x =[]
+class_labels_y = []
+reference_train_segments_x =[] 
+scaler = MinMaxScaler(feature_range=(0,2))
 
 
-print(f"Old cd for y_test:{sum(y_test)/y_test.shape[0] } ") 
-init_test_length = y_test.shape[0]
+for segment in tqdm(range(init_train_length)):
 
-for segment in tqdm(range(init_test_length//2)):
-    if y_test[segment] == 1:
-        scaler = MinMaxScaler(feature_range=(0,2))
-        scaled_X_segment = scaler.fit_transform(X_test[segment, :].reshape(-1,1))
-       # print(scaled_X_segment.shape)
-       # print(X_test.shape, y_test.shape)
+    scaled_X_segment = scaler.fit_transform(um_train_X[segment, :].reshape(-1,1)).squeeze()
 
-        X_test = np.vstack((X_test, scaled_X_segment.squeeze()))         
-        y_test = np.hstack((y_test, np.array(1))) 
+   #add newly scaled segment to the train set  
+    augmented_train_segments_x.append(scaled_X_segment)
+    class_labels_y.append(um_train_y[segment])
+    reference_train_segments_x.append(um_reference_train_X[segment,:] )
 
-       # print(X_test.shape, y_test.shape)
-        
+augmented_train_segments_x = np.array(augmented_train_segments_x, dtype=np.float32)
+class_labels_y = np.array(class_labels_y, dtype=np.float32)
+reference_train_segments_x = np.array(reference_train_segments_x, dtype=np.float32)
 
-print(f"New cd for y_test:{sum(y_test)/y_test.shape[0] } ")
+#add newly scaled segment to the train set  
+aum_train_X = np.vstack((um_train_X, augmented_train_segments_x))
+aum_train_y = np.hstack((um_train_y, class_labels_y)) 
+aum_reference_train_X = np.vstack((um_reference_train_X, reference_train_segments_x))         
 
-torch.save(X_test, "tensors/augmented_UM_test_X.pt")
-torch.save(y_test, "tensors/augmented UM_test_y.pt")
+
+torch.save(torch.Tensor(aum_train_X), "tensors/final_tensors_1703/augmented_um_train_X.pt")
+torch.save(torch.Tensor(aum_train_y), "tensors/final_tensors_1703/augmented um_train_y.pt")
+torch.save(torch.Tensor(aum_reference_train_X), "tensors/final_tensors_1703/augmented um_reference_train_X.pt")

nxm_quality_assessment.py
@@ -62,14 +62,16 @@ if __name__ == "__main__":
 
     parameters_dict = {
         'COMPENSATOR_ARCH': {
-            'values': ['fcn-dae', "fcn-dae-skip", "drdnn"]
+          #  'values': ['fcn-dae', "fcn-dae-skip", "drdnn"]
+            "values" :["fcn-dae-skip"] 
                 },
         'INIT_LR': {
-            "values" :[0.01, 0.0033, 0.001, 0.00033, 0.0001, 0.000033, 0.00001] 
-
+         #   "values" :[0.01, 0.0033, 0.001, 0.00033, 0.0001, 0.000033, 0.00001] 
+            "values" :[0.0001] 
             },
         "BATCH_SIZE":{
-            "values" :[1024, 2048, 4096, 8192] 
+         #   "values" :[1024, 2048, 4096, 8192] 
+            "values" :[2048] 
         }     
     } 
 
@@ -81,17 +83,21 @@ if __name__ == "__main__":
     # set the device we will be using to train the model
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    X_train = torch.load("tensors/final_tensors_1703/um_train_X.pt")
+    X_train = torch.load("tensors/final_tensors_1703/augmented_um_train_X.pt")
     X_test = torch.load("tensors/final_tensors_1703/um_test_X.pt")
-    X_train_reference = torch.load("tensors/final_tensors_1703/um_reference_train_X.pt")
+    X_train_reference = torch.load("tensors/final_tensors_1703/augmented_um_reference_train_X.pt")
     X_test_reference = torch.load("tensors/final_tensors_1703/um_reference_test_X.pt")
 
-    y_train = torch.load("tensors/final_tensors_1703/um_train_y.pt")
+    y_train = torch.load("tensors/final_tensors_1703/augmented_um_train_y.pt")
     y_test = torch.load("tensors/final_tensors_1703/um_test_y.pt")
 
     X_validation = torch.load("tensors/final_tensors_1703/um_validation_X.pt")
     y_validation = torch.load("tensors/final_tensors_1703/um_validation_y.pt")
     X_validation_reference = torch.load("tensors/final_tensors_1703/um_reference_validation_X.pt")
+
+
+
+
 
 
     """
@@ -136,29 +142,29 @@ if __name__ == "__main__":
    # compensator.train()
 
 
-    sweep_id = wandb.sweep(sweep_config, project="compensation-models-big-comparison-8-1-1")
+    sweep_id = wandb.sweep(sweep_config, project="augmented-dataset-comparison-run")
 
-    wandb.agent(sweep_id, compensator.train, count=50)
-    """    comp_results_train_loss, comp_results_val_loss = compensator.getRawResults()
-                
-        plt.plot(comp_results_train_loss, label='Train Loss')
-        plt.plot(comp_results_val_loss, label=' Val Loss')
+    wandb.agent(sweep_id, compensator.train, count=1)
+    comp_results_train_loss, comp_results_val_loss = compensator.getRawResults()
+            
+    plt.plot(comp_results_train_loss, label='Train Loss')
+    plt.plot(comp_results_val_loss, label=' Val Loss')
 
-        plt.xlabel('Epochs')
-        plt.ylabel("Loss")
-        plt.title('Loss')
-        plt.legend()
-        plt.savefig(f"plots/newdatasetLoss.png")
-        plt.clf()
+    plt.xlabel('Epochs')
+    plt.ylabel("Loss")
+    plt.title('Loss')
+    plt.legend()
+    plt.savefig(f"plots/newdatasetLoss.png")
+    plt.clf()
 
 
-        zero_idx_list = np.arange(2000,60000,100)
-        max_snaps = 100
-        snap_counter = 0
-        for i in zero_idx_list:
-            if snap_counter == max_snaps:
-                break        
-            compensator.getRandomSnapshot(random_seed=i)
-            snap_counter = snap_counter + 1"""
+    zero_idx_list = np.arange(2000,60000,100)
+    max_snaps = 100
+    snap_counter = 0
+    for i in zero_idx_list:
+        if snap_counter == max_snaps:
+            break        
+        compensator.getRandomSnapshot(random_seed=i)
+        snap_counter = snap_counter + 1
 
 